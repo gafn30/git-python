4e702f60d5ee06717ddbac76065d55fb66d57b84
Andew <andreikaagafonov30@gmail.com>
2022-12-31 19:49:51 +0300

    first commit

---
+++ Python/1.py
@@ -0,0 +1,9 @@
+# Найдите сумму цифр трехзначного числа.
+
+print('Введите a');
+a = int(input())
+print('Введите b');
+b = int(input())
+print('Введите c');
+c = int(input())
+print (a, '+' , b, '+', c , '=', a+b+c)
